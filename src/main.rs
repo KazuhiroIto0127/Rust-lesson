35faffc01a50c045db02df9185bfabdf1308fcb9
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-03-25 22:58:44 +0900

    mainにifとforの例を追加

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,24 @@ fn main() {
 
     let sum = func::add(10, 3);
     println!("The sum is: {}", sum);
+
+    // ============
+    // if else
+    // ============
+    let x = 5;
+
+    if x > 0 {
+        println!("x is positive");
+    } else {
+        println!("x is not positive");
+    }
+
+    // ==========
+    // for
+    // ==========
+    let numbers = [1, 2, 3, 4, 5];
+
+    for number in numbers.iter() {
+        println!("{}", number);
+    }
 }
